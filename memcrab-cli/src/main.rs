d6e11745581c4913ef062ef906ef68cb4fc00c04
immanelg <119798691+immanelg@users.noreply.github.com>
2024-01-03 16:41:47 +0300

    Add simple repl

--- memcrab-cli/src/main.rs
+++ memcrab-cli/src/main.rs
@@ -1,38 +1,63 @@
 use clap::Parser;
+use memcrab::pb::{cache_rpc_client::CacheRpcClient, GetRequest, SetRequest};
 use rustyline::error::ReadlineError;
-use rustyline::{DefaultEditor, Result};
+use rustyline::DefaultEditor;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    #[arg(short = 'H', long)]
-    host: Option<String>,
+    #[arg(short = 'H', long, default_value = "127.0.0.1")]
+    host: String,
 
-    #[arg(short, long)]
-    port: Option<String>,
+    #[arg(short, long, default_value = "9090")]
+    port: String,
 
     #[arg(short, long, action)]
-    interactive: bool,
+    server: bool,
 }
 
-fn eval_line(line: String) {
-    println!("you typed: {}", line);
-}
+// async fn eval_line(client: &mut CacheRpcClient, line: String) -> anyhow::Result<()> {
+//     Ok(())
+// }
 
-fn read_lines() -> Result<()> {
+// struct REPLSyntaxError;
+
+async fn eval_lines(addr: String) -> anyhow::Result<()> {
+    let mut client = CacheRpcClient::connect(addr).await.unwrap();
     let mut readline = DefaultEditor::new()?;
     loop {
         let readline = readline.readline("memcrab> ");
         match readline {
             Ok(line) => {
-                eval_line(line);
+                let tokens = line.split_whitespace().collect::<Vec<_>>();
+                if tokens[0] == "get" {
+                    let msg = GetRequest {
+                        key: tokens[1].to_owned(),
+                    };
+                    let req = tonic::Request::new(msg);
+                    let resp = client.get(req).await?.into_inner();
+                    match resp.value {
+                        Some(val) => println!("value: {:?}", val),
+                        None => println!("no value set"),
+                    }
+                } else if tokens[0] == "set" {
+                    let msg = SetRequest {
+                        key: tokens[1].to_owned(),
+                        value: tokens[2..]
+                            .iter()
+                            .map(|&s| s.parse().unwrap())
+                            .collect::<Vec<u8>>(),
+                    };
+                    let req = tonic::Request::new(msg);
+                    client.set(req).await?;
+                }
             }
             Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
+                println!("Ctrl-C");
                 continue;
             }
             Err(ReadlineError::Eof) => {
-                println!("CTRL-D");
+                println!("Quit");
                 break;
             }
             Err(err) => {
@@ -45,16 +70,12 @@ fn read_lines() -> Result<()> {
     Ok(())
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
-    if let Some(host) = cli.host.as_deref() {
-        println!("Value for host: {}", host);
-    }
-    if let Some(port) = cli.port.as_deref() {
-        println!("Value for port: {}", port);
-    }
-    if cli.interactive {
-        read_lines();
-    }
+    let dst_addr = format!("http://{}{}", cli.port, cli.host);
+
+    eval_lines(dst_addr).await?;
+    Ok(())
 }
