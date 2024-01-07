ae9eba101ed03addff518baa6200d8cd88140167
Alex Severin <severinalexeyv@gmail.com>
2024-01-08 01:11:52 +0300

    add async writer

--- memcrab-protocol/src/lib.rs
+++ memcrab-protocol/src/lib.rs
@@ -1,11 +1,8 @@
-mod channel;
 mod err;
-mod io;
+mod socket;
 
+pub mod io;
 #[allow(dead_code)]
 pub mod tokens;
 
-pub use channel::MemcrabChannel;
 pub use err::ProtocolError;
-
-pub use io::AsyncReader;
