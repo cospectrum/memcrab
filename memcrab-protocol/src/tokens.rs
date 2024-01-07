cdd3ee14abce2e19f96112c5a5695345ef567129
Alex Severin <severinalexeyv@gmail.com>
2024-01-07 23:34:50 +0300

    add basic tokens

--- memcrab-protocol/src/tokens.rs
+++ memcrab-protocol/src/tokens.rs
@@ -1,5 +1,42 @@
-#[derive(Debug, Clone)]
-pub enum Msg {
-    Get(Vec<u8>),
-    Set(Vec<u8>, Vec<u8>),
+use serde::{Deserialize, Serialize};
+
+// client writes this msg, server reads
+#[derive(Debug)]
+pub struct Request {
+    header: RequestHeader,
+    payload: Option<Vec<u8>>,
 }
+
+// server writes this msg, client reads
+#[derive(Debug)]
+pub struct Response {
+    header: ResponseHeader,
+    payload: Option<Vec<u8>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum RequestHeader {
+    Ping,
+    Get { klen: u64 },
+    Set { klen: u64, vlen: u64, expiration: Seconds },
+    Clear,
+    Delete { klen: u64 },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ResponseHeader {
+    Error(Error),
+    Pong,
+    Value { vlen: u64 },
+    KeyNotFound,
+    Ok,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Error {
+    Validation { len: u64 },
+    Internal { len: u64 },
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+pub struct Seconds(pub u64);
