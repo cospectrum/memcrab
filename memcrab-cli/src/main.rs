1fb6974739fa74847856e9b29da1f7d065edfbdf
immanelg <119798691+immanelg@users.noreply.github.com>
2024-03-08 00:34:28 +0300

    cli progress

--- memcrab-cli/src/main.rs
+++ memcrab-cli/src/main.rs
@@ -1,108 +1,109 @@
-// use anyhow::bail;
-// use clap::Parser;
-// use core::num::NonZeroUsize;
-// use memcrab::RawClient;
-// use memcrab_cache::Cache;
-// use rustyline::error::ReadlineError;
-// use rustyline::DefaultEditor;
-//
-// #[derive(Parser)]
-// #[command(author, version, about, long_about = None)]
-// struct Cli {
-//     #[arg(short = 'H', long, default_value = "127.0.0.1")]
-//     host: String,
-//
-//     #[arg(short, long, default_value = "9090")]
-//     port: String,
-//
-//     #[arg(short, long, action)]
-//     server: bool,
+use std::{net::SocketAddr, io};
+use std::net::Ipv4Addr;
+use anyhow::anyhow;
+use clap::{Args, Parser, Subcommand};
+use memcrab::{RawClient, connections::Tcp, Error};
+
+#[derive(Parser)]
+#[command(author, version, about = "command line interface for memcrab (server and REPL)", long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Server { 
+        // #[arg(short = 'H', long, default_value = "127.0.0.1")]
+        // host: Ipv4Addr,
+        //
+        // #[arg(short, long, default_value = "9090")]
+        // port: u16,
+
+        // TODO: PathBuf for UNIX Socket
+        #[arg(short, long, default_value = "localhost:9090")]
+        address: SocketAddr,
+    },
+    Client {
+        // #[arg(short = 'H', long, default_value = "127.0.0.1")]
+        // host: Ipv4Addr,
+        //
+        // #[arg(short, long, default_value = "9090")]
+        // port: u16,
+
+        #[arg(short, long, default_value = "localhost:9090")]
+        address: SocketAddr,
+
+        #[clap(num_args = 1.., value_delimiter = ' ', help = "execute command and exit")]
+        cmd: Vec<String>,
+    }
+}
+
+// async fn eval_line<C>(&mut client: RawClient<C>, line: String) -> io::Result<String> {
 // }
 //
-// async fn eval_lines(addr: String) -> anyhow::Result<()> {
-//     let addr = addr.parse()?;
-//     let mut client = RawClient::connect(addr).await?;
-//     let mut editor = DefaultEditor::new()?;
-//     loop {
-//         let line = editor.readline("memcrab> ");
-//         match line {
-//             Ok(line) => {
-//                 eval_line(&mut client, line)
-//                     .await
-//                     .unwrap_or_else(|e| println!("error: {:?}", e));
-//             }
-//             Err(ReadlineError::Interrupted) => {
-//                 println!("Ctrl-c");
-//             }
-//             Err(ReadlineError::Eof) => {
-//                 println!("quit");
-//                 break;
-//             }
-//             Err(err) => {
-//                 println!("error: {:?}", err);
-//                 break;
-//             }
-//         }
-//     }
-//
-//     Ok(())
-// }
-//
-// async fn eval_line(client: &mut RawClient, line: String) -> anyhow::Result<()> {
-//     let tokens = line.split_whitespace().collect::<Vec<_>>();
-//     if tokens.is_empty() {
-//         return Ok(());
-//     }
-//     if tokens[0] == "get" {
-//         if tokens.len() != 2 {
-//             bail!("syntax error: expected one key after `get`");
-//         }
-//         let resp = client.get(tokens[1]).await?;
-//         match resp {
-//             Some(val) => println!("{}: {:?}", tokens[1], val),
-//             None => println!("no value set"),
-//         }
-//     } else if tokens[0] == "set" {
-//         if tokens.len() < 3 {
-//             bail!("syntax error: expected one key and bytes after `set`");
-//         }
-//
-//         client
-//             .set(
-//                 tokens[1],
-//                 tokens[2..]
-//                     .iter()
-//                     .map(|&s| s.parse().unwrap())
-//                     .collect::<Vec<u8>>(),
-//             )
-//             .await?;
-//     } else {
-//         bail!("syntax error: unexpected token {}", tokens[0]);
-//     }
-//     Ok(())
-// }
-//
-// #[allow(unused)]
-// async fn serve(addr: String) -> anyhow::Result<()> {
-//     let maxbytes = 100_000;
-//     let maxlen = NonZeroUsize::new(110).unwrap();
-//     let cache = Cache::new(maxlen, maxbytes);
-//
-//     todo!("server is not implemented");
-//     Ok(())
-// }
-//
-// #[tokio::main]
-// async fn main() -> anyhow::Result<()> {
-//     let cli = Cli::parse();
-//
-//     let addr = format!("{}:{}", cli.host, cli.port);
-//
-//     if cli.server {
-//         serve(addr).await?;
-//     } else {
-//         eval_lines(addr).await?;
-//     }
-//     Ok(())
-// }
-fn main() {}
+
+async fn serve_memcrab(addr: SocketAddr) -> anyhow::Result<()> {
+    todo!();
+}
+
+
+async fn repl<C: memcrab::Rpc>(mut client: RawClient<C>) -> anyhow::Result<()> {
+    let mut editor = rustyline::DefaultEditor::new()?;
+
+    for readline in editor.iter("📝🦀  ") {
+        match readline {
+            Ok(line) => { 
+                let err = eval_line(&mut client, line.split_whitespace()/*.map(|s| s.to_string())*/.collect()).await;
+                println!("error: {:?}", err);
+            },
+            Err(rustyline::error::ReadlineError::Eof) => {
+                println!("quit");
+                break;
+            }
+            Err(e) => println!("error: {}", e),
+        }
+    }
+    Ok(())
+}
+
+async fn eval_line<C: memcrab::Rpc>(client: &mut RawClient<C>, line: Vec<&str>) -> anyhow::Result<String> {
+    match line[..] {
+        ["get", string_key] => {
+            // TODO:
+            // "quoted key with spaces"
+            let value = client.get(string_key).await?;
+            match value {
+                Some(value) => {
+                    let string_value = String::from_utf8(value).unwrap();
+                    Ok(string_value)
+                }
+                None => {
+                    Ok(String::from("key not found"))
+                }
+            }
+        }
+        _ => Err(anyhow!("Syntax error"))
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Client { address, cmd } => {
+            let mut client = RawClient::<Tcp>::connect(address).await?;
+            if cmd.is_empty() {
+                repl(client).await?;
+            } else {
+                eval_line(&mut client, cmd.iter().map(|s| s.as_str()).collect()).await?;
+            }
+        }
+        Commands::Server { address } => {
+            serve_memcrab(address).await?;
+        }
+    }
+
+    Ok(())
+}
